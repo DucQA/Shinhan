37f93ccf193a8bed15c5fc081cda36d0b8a647a8
dviet9968 <dviet@outlook.com>
2025-06-22 23:08:53 +0700

    Update TransferPage.java

--- src/test/java/ui/TransferPage.java
+++ src/test/java/ui/TransferPage.java
@@ -6,22 +6,31 @@ import net.serenitybdd.screenplay.targets.Target;
 public class TransferPage {
     public static Target bankNameDropdownBtn = Target.the("Bank Name Dropdown Button")
             .locatedBy("(//XCUIElementTypeImage[@name='icAngleDownBlack'])[2]");
+
     public static Target accountNumberField = Target.the("Account Number Field")
             .located(AppiumBy.accessibilityId("Account number"));
+
     public static Target transferAmountField = Target.the("Transfer Amount Field")
             .located(AppiumBy.accessibilityId("Transfer amount"));
+
     public static Target searchField = Target.the("Search Field")
             .located(AppiumBy.accessibilityId("Search here"));
+
     public static Target nextButton = Target.the("Next Button")
             .located(AppiumBy.accessibilityId("Next"));
+
     public static Target confirmButton = Target.the("Confirm Button")
             .located(AppiumBy.accessibilityId("Confirm"));
+
     public static Target keyboardReturnButton = Target.the("Keyboard Return Button")
             .located(AppiumBy.accessibilityId("Return"));
+
     public static Target keyboardDoneButton = Target.the("Keyboard Return Button")
             .located(AppiumBy.accessibilityId("Done"));
+
     public static Target backButton = Target.the("Back Button")
             .located(AppiumBy.accessibilityId("icArrowLeftWhite"));
+
     public static Target transferToOthersButton = Target.the("Transfer to Others Button")
             .located(AppiumBy.accessibilityId("Transfer to others"));
 
